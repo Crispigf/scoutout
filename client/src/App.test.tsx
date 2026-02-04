4b18bf7654de658f35da81b2cd99266a32120e79
nikolai <144839823+unitnikolai@users.noreply.github.com>
2026-02-03 20:10:30 -0500

    test

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders quantum news dashboard', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Quantum News/i);
-  expect(headerElement).toBeInTheDocument();
-});
